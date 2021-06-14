8c35df46a554fa745e23f3d7675b35c7f28c5f48
negbook <negbook@gmail.com>
2021-06-14 16:22:33 +0800

    Update client.lua

--- nbk_circle/client.lua
+++ nbk_circle/client.lua
@@ -32,3 +32,47 @@ CreateThread(function()
         end
 
 end)
+--[[
+function GetMinimapAnchor()
+    -- Safezone goes from 1.0 (no gap) to 0.9 (5% gap (1/20))
+    -- 0.05 * ((safezone - 0.9) * 10)
+    local safezone = GetSafeZoneSize()
+    local safezone_x = 1.0 / 20.0
+    local safezone_y = 1.0 / 20.0
+    local aspect_ratio = GetAspectRatio(0)
+    local res_x, res_y = GetActiveScreenResolution()
+    local xscale = 1.0 / res_x
+    local yscale = 1.0 / res_y
+    local Minimap = {}
+    Minimap.width = xscale * (res_x / (4 * aspect_ratio))
+    Minimap.height = yscale * (res_y / 6.2) *1.33
+    SetScriptGfxAlign(string.byte('L'), string.byte('B')) --https://forum.cfx.re/t/useful-snippet-getting-the-top-left-of-the-minimap-in-screen-coordinates/712843
+    Minimap.left_x, Minimap.top_y = GetScriptGfxPosition(-0.0045+0.004, (0.002 + (-0.188888)+0.008)*1.33)
+    ResetScriptGfxAlign()
+    --Minimap.left_x = xscale * (res_x * (safezone_x * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.bottom_y = 1.0 - yscale * (res_y * (safezone_y * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.right_x = Minimap.left_x + Minimap.width
+    --Minimap.top_y = Minimap.bottom_y - Minimap.height
+    Minimap.x = Minimap.left_x
+    Minimap.y = Minimap.top_y
+    Minimap.xunit = xscale
+    Minimap.yunit = yscale
+    return Minimap
+end
+function drawRct(x, y, width, height, r, g, b, a)
+    DrawRect(x + width/2, y + height/2, width, height, r, g, b, a)
+end
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        local ui = GetMinimapAnchor()
+        local thickness = 1 -- Defines how many pixels wide the border is
+        drawRct(ui.x, ui.y, ui.width, thickness * ui.yunit, 0, 0, 0, 255)
+        drawRct(ui.x, ui.y + ui.height, ui.width, -thickness * ui.yunit, 0, 0, 0, 255)
+        drawRct(ui.x, ui.y, thickness * ui.xunit, ui.height, 0, 0, 0, 255)
+        drawRct(ui.x + ui.width, ui.y, -thickness * ui.xunit, ui.height, 0, 0, 0, 255)
+    end
+end)
+
+
+]]--
